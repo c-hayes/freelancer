794c0953bc48923417917e9b1d8fc93683a2e443
c-hayes <chayes2913@gmail.com>
2023-05-31 14:42:18 -0600

    email plugin

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-// template_bcgz8aw
+// template_2e4ixts
 // service_5krgatp
-// key jECY0IM7sQ262RF4z5fh7
-
+// private-key jECY0IM7sQ262RF4z5fh7
 // Emailjs.com
 
-
 function contact(event) {
-    event.preventDefault();
-    const loading = document.querySelector(".modal__overlay--loading");
-    const success = document.querySelector(".modal__overlay--success");
-    loading.classList += " modal__overlay--visible";
-    emailjs.sendForm(
-            'service_5krgatp',
-            'template_bcgz8aw',
-            event.target,
-            'jECY0IM7sQ262RF4z5fh7'
-      )
-      .then(() => {
-        loading.classList.remove("modal__overlay--visible");
-        success.classList += " modal__overlay--visible";
-      })
-      .catch(() => {
-        loading.classList.remove("modal__overlay--visible");
-        alert(
-          "The email service is temporarily unavailable. Please contact us directly at phconstruction@sbcglobal.net"
-        );
-      });
-  }
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+  .sendForm(
+        'service_5krgatp',
+        'template_2e4ixts',
+        event.target,
+        'jECY0IM7sQ262RF4z5fh7'
+  )
+  .then(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+  })
+  .catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert(
+      "The email service is temporarily unavailable. Please contact us directly phconstruction@sbcglobal.net or by phone 405.257.3307"
+    );
+  });
+}
 
 let isModalOpen = false;
 function toggleModal(){
-    if (isModalOpen){
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open");
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open";
+if (isModalOpen){
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+}
+isModalOpen = true;
+document.body.classList += " modal--open";
 }
 
 let contrastLightMode = true;
 function toggleContrast(){
-    contrastLightMode = !contrastLightMode;
-    if(!contrastLightMode){
-        document.body.classList += " dark-theme"
-    }
-    else{
-        document.body.classList.remove("dark-theme")
-    }
+contrastLightMode = !contrastLightMode;
+if(!contrastLightMode){
+    document.body.classList += " dark-theme"
 }
+else{
+    document.body.classList.remove("dark-theme")
+}
+}
+
