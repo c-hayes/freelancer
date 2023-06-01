1bdad8772c5ab97a6ddaf472b325f0a0b5779f33
c-hayes <chayes2913@gmail.com>
2023-06-01 11:37:52 -0600

    final edits to first draft

--- index.js
+++ index.js
@@ -10,42 +10,40 @@ function contact(event) {
   const success = document.querySelector(".modal__overlay--success");
   loading.classList += " modal__overlay--visible";
   emailjs
-  .sendForm(
-        'service_5krgatp',
-        'template_kualxif',
-        event.target,
-        'qFmLqTmUSlAis_HgQ'
-  )
-  .then(() => {
-    loading.classList.remove("modal__overlay--visible");
-    success.classList += " modal__overlay--visible";
-  })
-  .catch(() => {
-    loading.classList.remove("modal__overlay--visible");
-    alert(
-      "The email service is temporarily unavailable. Please contact us directly phconstruction@sbcglobal.net or by phone 405.257.3307"
-    );
-  });
+    .sendForm(
+      "service_5krgatp",
+      "template_kualxif",
+      event.target,
+      "qFmLqTmUSlAis_HgQ"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact us directly phconstruction@sbcglobal.net or by phone 405.257.3307"
+      );
+    });
 }
 
 let isModalOpen = false;
-function toggleModal(){
-if (isModalOpen){
+function toggleModal() {
+  if (isModalOpen) {
     isModalOpen = false;
     return document.body.classList.remove("modal--open");
-}
-isModalOpen = true;
-document.body.classList += " modal--open";
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
 
 let contrastLightMode = true;
-function toggleContrast(){
-contrastLightMode = !contrastLightMode;
-if(!contrastLightMode){
-    document.body.classList += " dark-theme"
+function toggleContrast() {
+  contrastLightMode = !contrastLightMode;
+  if (!contrastLightMode) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
 }
-else{
-    document.body.classList.remove("dark-theme")
-}
-}
-
